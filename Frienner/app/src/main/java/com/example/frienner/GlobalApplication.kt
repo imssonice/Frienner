0ae04c03e7ad486bc786c53d1697555594a1a503
youhyeon <nanana126@ajou.ac.kr>
2024-08-14 15:07:35 +0900

    log in first complete

--- Frienner/app/src/main/java/com/example/frienner/GlobalApplication.kt
+++ Frienner/app/src/main/java/com/example/frienner/GlobalApplication.kt
@@ -1,6 +1,7 @@
 package com.example.frienner
 import android.app.Application
 import android.util.Log
+import com.kakao.sdk.common.KakaoSdk
 import com.kakao.vectormap.KakaoMapSdk
 import com.kakao.sdk.common.util.Utility
 
@@ -10,10 +11,11 @@ class GlobalApplication : Application() {
         // Kakao SDK 초기화
 
         KakaoMapSdk.init(this, "290707713f1b1a8c4e38949450f84fb3")
+        // Kakao SDK 초기화
+        KakaoSdk.init(this, "290707713f1b1a8c4e38949450f84fb3")
 
+        //var keyHash = Utility.getKeyHash(this)
 
-        var keyHash = Utility.getKeyHash(this)
-
-        Log.d("keyHash", "keyHash: $keyHash")
+        //Log.d("keyHash", "keyHash: $keyHash")
     }
 }
--- Frienner/app/src/main/java/com/example/frienner/Login/SignInActivity.kt
+++ Frienner/app/src/main/java/com/example/frienner/Login/SignInActivity.kt
@@ -1,73 +1,65 @@
-//package com.example.frienner.Login
-//
-//import android.content.Intent
-//import android.os.Bundle
-//import android.widget.Button
-//import android.widget.EditText
-//import android.widget.Toast
-//import androidx.activity.result.ActivityResultLauncher
-//import androidx.activity.result.contract.ActivityResultContracts
-//import androidx.appcompat.app.AppCompatActivity
-//import com.example.frienner.R
-//import com.kakao.sdk.user.UserApiClient
-//
-//class SignInActivity : AppCompatActivity() {
-//
-//    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_sign_in)
-//
-//        val et_id = findViewById<EditText>(R.id.et_id)
-//        val et_pw = findViewById<EditText>(R.id.et_pw)
-//        val btn_login = findViewById<Button>(R.id.btn_login)
-//        val btn_signUp = findViewById<Button>(R.id.btn_signup)
-//        val btn_kakao_login = findViewById<Button>(R.id.btn_kakao_login)
-//
-//        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-//            if (result.resultCode == Activity.RESULT_OK) {
-//                val user_id = result.data?.getStringExtra("id") ?: ""
-//                val user_pw = result.data?.getStringExtra("pw") ?: ""
-//                et_id.setText(user_id)
-//                et_pw.setText(user_pw)
-//            }
-//        }
-//
-//        btn_login.setOnClickListener {
-//            if (et_id.text.toString().trim().isEmpty() || et_pw.text.toString().trim().isEmpty()) {
-//                Toast.makeText(this, getString(R.string.toast_msg_idpqErr), Toast.LENGTH_SHORT).show()
-//                return@setOnClickListener
-//            }
-//            // 로그인 로직
-//        }
-//
-//        btn_signUp.setOnClickListener {
-//            val intent = Intent(this, SignUpActivity::class.java)
-//            activityResultLauncher.launch(intent)
-//        }
-//
-//        btn_kakao_login.setOnClickListener {
-//            // KakaoTalk 로그인
-//            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
-//                UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
-//                    if (error != null) {
-//                        Toast.makeText(this, "로그인 실패: ${error.message}", Toast.LENGTH_SHORT).show()
-//                    } else if (token != null) {
-//                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
-//                        // 로그인 성공 후 처리
-//                    }
-//                }
-//            } else {
-//                UserApiClient.instance.loginWithKakaoAccount(this) { token, error ->
-//                    if (error != null) {
-//                        Toast.makeText(this, "로그인 실패: ${error.message}", Toast.LENGTH_SHORT).show()
-//                    } else if (token != null) {
-//                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
-//                        // 로그인 성공 후 처리
-//                    }
-//                }
-//            }
-//        }
-//    }
-//}
+package com.example.frienner.Login
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.frienner.R
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
+
+class SignInActivity : AppCompatActivity() {
+
+    // 이메일 로그인 콜백
+    private val mCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+        if (error != null) {
+            Log.e("TAG", "로그인 실패 $error")
+        } else if (token != null) {
+            Log.e("TAG", "로그인 성공 ${token.accessToken}")
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        val btn_kakao_login = findViewById<Button>(R.id.btn_signin_kakao)
+
+        btn_kakao_login.setOnClickListener {
+            // 카카오톡 설치 확인
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+                // 카카오톡 로그인
+                UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+                    // 로그인 실패 부분
+                    if (error != null) {
+                        Log.e("TAG", "로그인 실패 $error")
+                        // 사용자가 취소
+                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled ) {
+                            return@loginWithKakaoTalk
+                        }
+                        // 다른 오류
+                        else {
+                            UserApiClient.instance.loginWithKakaoAccount(this, callback = mCallback) // 카카오 이메일 로그인
+                        }
+                    }
+                    // 로그인 성공 부분
+                    else if (token != null) {
+                        Log.e("TAG", "로그인 성공 ${token.accessToken}")
+                    }
+                }
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(this, callback = mCallback) // 카카오 이메일 로그인
+            }
+
+        }
+
+
+    }
+
+
+
+
+}
